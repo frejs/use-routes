01be16c9cdad0bb4c799b8d2379b52f9598a8186
132yse <1533540012@qq.com>
2019-03-22 15:48:50 +0800

    打包发版

--- dist/use-routes.js
+++ dist/use-routes.js
@@ -6,13 +6,29 @@
 
   React = React && React.hasOwnProperty('default') ? React['default'] : React;
 
+  function _extends() {
+    _extends = Object.assign || function (target) {
+      for (var i = 1; i < arguments.length; i++) {
+        var source = arguments[i];
+
+        for (var key in source) {
+          if (Object.prototype.hasOwnProperty.call(source, key)) {
+            target[key] = source[key];
+          }
+        }
+      }
+
+      return target;
+    };
+
+    return _extends.apply(this, arguments);
+  }
+
   let stack = {};
   let prepared = {};
-
-  function useRoutes (routes) {
+  function useRoutes(routes) {
     const [rid] = React.useState(Math.random().toString());
     const setter = React.useState(0)[1];
-
     let stackObj = stack[rid];
 
     if (!stackObj) {
@@ -20,84 +36,84 @@
         routes: Object.entries(routes),
         setter
       };
-
       stack[rid] = stackObj;
-
       process(rid);
     }
 
-    const result = stackObj.component(stackObj.props);
-
-    return result
+    return typeof stackObj.component === 'function' ? stackObj.component(stackObj.props) : push(stackObj.component);
   }
 
-  function process (rid) {
-    const { routes, setter, path: oldPath } = stack[rid];
+  function process(rid) {
+    const {
+      routes,
+      setter
+    } = stack[rid];
     const currentPath = location.pathname || '/';
-
-    let route, component, props, path;
+    let path, component, props;
 
     for (let i = 0; i < routes.length; i++) {
-  [route, component] = routes[i];
-      const [reg, group] = prepared[route]
-        ? prepared[route]
-        : preparedRoute(route);
-
+      [path, component] = routes[i];
+      const [reg, group] = prepared[path] ? prepared[path] : preparedRoute(path);
       const result = currentPath.match(reg);
+
       if (!result) {
         component = () => {};
-        continue
+
+        continue;
       }
 
       if (group.length) {
         props = {};
-        group.forEach((item, index) => {
-          props[item] = result[index + 1];
-        });
+        group.forEach((item, index) => props[item] = result[index + 1]);
       }
 
-      path = currentPath.replace(result[0], '');
-      break
+      break;
     }
 
     Object.assign(stack[rid], {
-      component,
-      props,
       path,
-      route
+      component,
+      props
     });
-
     setter(Date.now());
   }
 
-  function preparedRoute (route) {
-    if (prepared[route]) return prepared[route]
-    const prepare = [
-      new RegExp(
-        `${route.substr(0, 1) === '*' ? '' : '^'}${route
-        .replace(/:[a-zA-Z]+/g, '([^/]+)')
-        .replace(/\*/g, '')}${route.substr(-1) === '*' ? '' : '$'}`
-      )
-    ];
-
+  function preparedRoute(route) {
+    if (prepared[route]) return prepared[route];
+    const prepare = [new RegExp(`${route.substr(0, 1) === '*' ? '' : '^'}${route.replace(/:[a-zA-Z]+/g, '([^/]+)').replace(/\*/g, '')}${route.substr(-1) === '*' ? '' : '$'}`)];
     const props = route.match(/:[a-zA-Z]+/g);
     prepare.push(props ? props.map(name => name.substr(1)) : []);
-
     prepared[route] = prepare;
-    return prepare
+    return prepare;
   }
 
-  const push = url => {
+  function push(url) {
     window.history.pushState(null, null, url);
     processStack();
-  };
+  }
 
   const processStack = () => Object.keys(stack).forEach(process);
 
   window.addEventListener('popstate', processStack);
+  function A(props) {
+    const {
+      onClick: onclick
+    } = props;
+
+    const onClick = e => {
+      e.preventDefault();
+      push(e.target.href);
+      if (onclick) onclick(e);
+    };
+
+    return React.createElement("a", _extends({}, props, {
+      onClick: onClick
+    }));
+  }
 
   exports.useRoutes = useRoutes;
   exports.push = push;
+  exports.A = A;
 
   Object.defineProperty(exports, '__esModule', { value: true });
 
--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,16 @@
+import babel from 'rollup-plugin-babel'
+
 export default {
   input: './src/index.js',
   output: {
     file: './dist/use-routes.js',
     format: 'umd',
     name: 'use-routes'
-  }
+  },
+  plugins: [
+    babel({
+      babelrc: false,
+      presets: [['@babel/preset-react']],
+    })
+  ]
 }
