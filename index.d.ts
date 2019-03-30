ebbf24d3dd4376fa6623849d6fb863729e428425
Nanasaki <40260564+Saber2pr@users.noreply.github.com>
2019-03-30 13:33:21 +0000

    move to /

---
+++ index.d.ts
@@ -0,0 +1,45 @@
+/// <reference types="react" />
+/**
+ * Routes
+ *
+ * @export
+ * @interface Routes
+ */
+export interface Routes {
+  [url: string]: () => JSX.Element
+}
+/**
+ * useRoutes
+ *
+ * @export
+ * @param {Routes} routes
+ * @returns {(JSX.Element | null)}
+ */
+export function useRoutes(routes: Routes): JSX.Element | null
+/**
+ * push
+ *
+ * @export
+ * @param {string} url
+ */
+export function push(url: string): void
+/**
+ * AnchorProps
+ *
+ * @export
+ * @interface AnchorProps
+ * @extends {React.DetailedHTMLProps<React.AnchorHTMLAttributes<HTMLAnchorElement>, HTMLAnchorElement>}
+ */
+export interface AnchorProps
+  extends React.DetailedHTMLProps<
+    React.AnchorHTMLAttributes<HTMLAnchorElement>,
+    HTMLAnchorElement
+  > {}
+/**
+ * A
+ *
+ * @export
+ * @param {AnchorProps} props
+ * @returns {JSX.Element}
+ */
+export function A(props: AnchorProps): JSX.Element
