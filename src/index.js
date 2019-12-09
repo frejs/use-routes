35d32593d4d4f4158428620a4fefe18a22efe6de
伊撒尔 <1533540012@qq.com>
2019-12-09 16:23:06 +0800

    refactor complete

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-import { h, useState } from 'fre'
+import {h, useState} from 'fre'
+
 let routeStack = {}
 let pathCache = {}
 
-export function useRoutes (routes) {
+export function useRoutes(routes) {
   const id = useState(Symbol())[0]
   const setter = useState(0)[1]
 
@@ -12,37 +13,39 @@ export function useRoutes (routes) {
   }
 
   routeStack[id] = stack
-  perfrom()
+  perfrom(id)
 
   return typeof stack.component === 'string'
     ? push(stack.component)
     : stack.component(stack.props)
 }
 
-function perfrom (rid) {
-  const { routes, setter } = routeStack[rid]
+function perfrom(id) {
+  const {routes, setter} = routeStack[id]
   const currentPath = location.pathname || '/'
-
   let path, component, props
 
   for (let i = 0; i < routes.length; i++) {
-    ;[path, component] = routes[i]
+    const route = routes[i]
+    path = route[0]
+    component = route[1]
     const [reg, params] = pathSlice(path)
 
     const res = currentPath.match(reg)
     if (!res) {
-      component = () => {}
+      component = () => {
+      }
       continue
     }
 
     if (params.length) {
       props = {}
-      group.forEach((item, index) => (props[item] = result[index + 1]))
+      params.forEach((item, index) => (props[item] = res[index + 1]))
     }
     break
   }
 
-  Object.assign(routeStack[rid], {
+  Object.assign(routeStack[id], {
     path,
     component,
     props
@@ -68,22 +71,23 @@ function pathSlice(path) {
   return slice
 }
 
-export function push (url) {
+export function push(url) {
   window.history.pushState(null, null, url)
   processStack()
 }
 
-const processStack = () => Object.keys(routeStack).forEach(perfrom)
+const processStack = () =>{
+  Object.getOwnPropertySymbols(routeStack).forEach(perfrom)
+}
 
 window.addEventListener('popstate', processStack)
 
-export function A (props) {
-  const { onClick: onclick, children } = props
+export function A(props) {
+  const {onClick: onclick, children} = props
 
   const onClick = e => {
     e.preventDefault()
     push(e.target.href)
-
     if (onclick) onclick(e)
   }
 
